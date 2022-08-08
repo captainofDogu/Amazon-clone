cc1a30f65535658ee629fd415ed973f1d5b61453
doguhan önder <doguhan@doguhan-MacBook-Pro.local>
2022-08-08 15:51:24 +0300

    proje sepet eklendi

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAzH_WOVVDxfhTKQXig1N5_9emSq7spivs",
+  authDomain: "clone-dda49.firebaseapp.com",
+  projectId: "clone-dda49",
+  storageBucket: "clone-dda49.appspot.com",
+  messagingSenderId: "6138768080",
+  appId: "1:6138768080:web:3ba04ae63b146f48178cd5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
